5cef040eb7bb7d82fcd385c05a29a6c6939f12a2
Thomas P Minka <tpminka@media.mit.edu>
2011-01-17 00:00:00 +0000

    version 1.1-8.3

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,7 @@
 .First.lib <- function(lib, pkg) {
   library.dynam("mapproj", pkg, lib)
+# RSB 110117 remove external reference NOTE to .map.range
+  xx <- try(get(".map.range", env=globalenv()), silent=TRUE)
+  if (class(xx) == "try-error")
+    assign(".map.range", as.numeric(c(0, 1, 0, 1)), envir = globalenv())
 }
